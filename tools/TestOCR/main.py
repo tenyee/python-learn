0429cf43575a43c92a1d2a303ae6093f54e39587
tenyee <15380410441@163.com>
2018-07-11 15:22:35 +0800

    copy file

--- tools/TestOCR/main.py
+++ tools/TestOCR/main.py
@@ -1,20 +1,31 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-
+'''
+功能：
+提取目录下的图片文件到一个指定的根目录，保留文件的所在目录路径
+'''
 import os
 import re
 import shutil
 
-gRoot = '/Users/tenyee/workspace/2018/prj-python-learn'
+gRoot = r'C:\Users\vmp\czw\python-learn'
 gPattern = '.png|.jpg|.bmp'
-gDir = './pic'
+gDir = 'pic'
+#图片转存根目录 
+gTarget = r'C:\Users\vmp\czw\python-learn\tools\TestOCR\pic'
+print("所有图片将转存到此根目录：", gTarget)
 
 def GetImagePaths(root):
 	files = []
+
 	for fpath, dirs, flist in os.walk(root, topdown=True):
 		for i in flist:
 			path = os.path.join(fpath, i)
-			files.append(path)
+			tmp = {}
+			tmp["dir"] = fpath
+			tmp["name"] = i
+			files.append(tmp)
+
 	return files
 
 def CheckImages(path, types):
@@ -24,18 +35,18 @@ def CheckImages(path, types):
 	else:
 		return False
 
-if os.path.exists(gDir):
-	shutil.rmtree(gDir)
-
-os.mkdir(gDir)
+if os.path.exists(gTarget):
+	shutil.rmtree(gTarget)
 
 files = GetImagePaths(gRoot)
 for i in files:
-	if CheckImages(i, gPattern):
-		shutil.copy(i, gDir)
-
-
-
-
+	filePath = os.path.join(i["dir"], i["name"])
+	if CheckImages(filePath, gPattern):
+		dir = i["dir"]
+		targetDir = dir.replace(gRoot, gTarget)
+		if not os.path.exists(targetDir):
+			os.makedirs(targetDir)
+		targetPath = os.path.join(targetDir, i["name"])
+		shutil.copy(filePath, targetPath)
 
 
